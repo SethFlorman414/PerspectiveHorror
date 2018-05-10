ec4a4c743880c0a0e46f99353b86a4e8d9656e43
Maxwell Fischer <19.maxwell.fischer@ksd.org>
2018-05-10 13:42:46 -0700

    AAA

--- Horror/Assets/Scripts/doorInteraction.cs
+++ Horror/Assets/Scripts/doorInteraction.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class doorInteraction : MonoBehaviour {
 
-	
-	
+    public string boolName;
+    public float secondsUntilDoorCloses;
+    bool open;
+    float timer;
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+    {
+		if(open)
+        {
+            timer += Time.deltaTime;
+            if(timer >= secondsUntilDoorCloses)
+            {
+                timer = 0;
+                open = false;
+                GetComponent<Animator>().SetBool(boolName, false);
+            }
+        }
 	}
 
     public void interact()
     {
-
+        GetComponent<Animator>().SetBool(boolName, true);
     }
 }
