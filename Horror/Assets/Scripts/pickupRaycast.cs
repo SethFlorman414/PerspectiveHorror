c2701819d73fc8f9919d7b1cfbb83f358e04b20b
Maxwell Fischer <19.maxwell.fischer@ksd.org>
2018-05-10 12:22:42 -0700

    Raycast interact

--- Horror/Assets/Scripts/pickupRaycast.cs
+++ Horror/Assets/Scripts/pickupRaycast.cs
@@ -5,11 +5,22 @@ using UnityEngine;
 public class pickupRaycast : MonoBehaviour {
 
 
-	
-	
 	void Update ()
     {
-		
+        Camera cam = Camera.main;
+        RaycastHit hit;
+        Ray ray = cam.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+        if(Physics.Raycast(ray,out hit))
+        {
+            if (hit.collider.tag == "Interact")
+            {
+                //text or something to say you can interact
+                if (Input.GetButtonDown("Fire1"))
+                {
+
+                }
+            }
+        }
 	}
 
 
