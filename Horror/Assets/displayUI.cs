150b00a6dc63c7e3bacf274808983f81c5a221cd
Seth Florman <18.seth.florman@ksd.org>
2018-05-11 13:36:10 -0700

    More Level Design Stuff. SF

---
+++ Horror/Assets/displayUI.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class displayUI : MonoBehaviour {
+
+	public string myString;
+	public Text myText;
+	public float fadeTime;
+	public bool displayInfo;
+
+
+	// Use this for initialization
+	void Start () {
+		myText = gameObject.GetComponentInChildren<Text> ();//GameObject.Find ("Text").GetComponent<Text> ();
+		myText.color = Color.clear;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		FadeText ();
+
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			Screen.lockCursor = false;
+		}
+	}
+
+	void OnMouseOver(){
+		displayInfo = true;
+	}
+
+	void OnMouseExit(){
+		displayInfo = false;
+	}
+
+	void FadeText(){
+		if (displayInfo) {
+			myText.text = myString;
+			myText.color = Color.Lerp (myText.color, Color.white, fadeTime * Time.deltaTime);
+		} else {
+			myText.color = Color.Lerp (myText.color, Color.clear, fadeTime * Time.deltaTime);
+		}
+	}
+}
