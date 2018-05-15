0e87f080356fe54736e7edf4fefbd516471b9c04
Talia Avery <18.talia.avery@ksd.org>
2018-05-15 13:58:26 -0700

    UI

---
+++ Horror/Assets/Tyler/SanityUI.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SanityUI : MonoBehaviour
+{
+    Image m_Image;
+
+    public Sprite One_Sanity;
+    public Sprite Two_Sanity;
+    public Sprite Three_Sanity;
+    public Sprite Four_Sanity;
+    public Sprite Five_Sanity;
+
+    // Use this for initialization
+	void Start ()
+    {
+        m_Image = GetComponent <Image> ();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            m_Image.sprite = One_Sanity;
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            m_Image.sprite = Two_Sanity;
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            m_Image.sprite = Three_Sanity;
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            m_Image.sprite = Four_Sanity;
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            m_Image.sprite = Five_Sanity;
+        }
+    }
+}
