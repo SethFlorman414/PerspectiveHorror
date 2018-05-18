595e9d6bf8e1fe9ca7d27462592d7e7290974ce8
Talia Avery <18.talia.avery@ksd.org>
2018-05-18 13:59:18 -0700

    UI

---
+++ Horror/Assets/Tyler/Scripts/SanitySlider.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SanitySlider : MonoBehaviour
+{
+    Slider slider;
+
+    int sanity = 250;
+
+    public Image Fill;
+
+    public Color MaxColor = Color.green;
+    public Color MinColor = Color.red;
+	
+    void Start()
+    {
+        slider = gameObject.GetComponent<Slider>();
+
+
+        slider.wholeNumbers = true;
+        slider.minValue = 0f;
+        slider.maxValue = sanity;
+        slider.value = sanity;
+    }
+
+    void Update ()
+    {
+        slider.value = sanity;
+        Fill.color = Color.Lerp(MaxColor, MinColor, Mathf.PingPong(Time.time, 1));
+
+        sanity--;
+    }
+}
--- Horror/Assets/Tyler/Scripts/ScrollRectAutoScroll.cs
+++ Horror/Assets/Tyler/Scripts/ScrollRectAutoScroll.cs
@@ -8,85 +8,67 @@ using UnityEngine.UI;
 
 public class ScrollRectAutoScroll : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
-    public float scrollSpeed = 10f;
-    private bool mouseOver = false;
+    RectTransform scrollRectTransform;
+    RectTransform contentPanel;
+    RectTransform selectedRectTransform;
+    GameObject lastSelected;
 
-    private List<Selectable> m_Selectables = new List<Selectable>();
-    private ScrollRect m_ScrollRect;
+    Vector2 targetPos;
 
-    private Vector2 m_NextScrollPosition = Vector2.up;
-    void OnEnable()
-    {
-        if (m_ScrollRect)
-        {
-            m_ScrollRect.content.GetComponentsInChildren(m_Selectables);
-        }
-    }
-    void Awake()
-    {
-        m_ScrollRect = GetComponent<ScrollRect>();
-    }
     void Start()
     {
-        if (m_ScrollRect)
-        {
-            m_ScrollRect.content.GetComponentsInChildren(m_Selectables);
-        }
-        ScrollToSelected(true);
+        scrollRectTransform = GetComponent<RectTransform>();
+
+        if (contentPanel == null)
+            contentPanel = GetComponent<ScrollRect>().content;
+
+        targetPos = contentPanel.anchoredPosition;
     }
+
     void Update()
     {
-        // Scroll via input.
-        InputScroll();
-        if (!mouseOver)
-        {
-            // Lerp scrolling code.
-            m_ScrollRect.normalizedPosition = Vector2.Lerp(m_ScrollRect.normalizedPosition, m_NextScrollPosition, scrollSpeed * Time.deltaTime);
-        }
-        else
-        {
-            m_NextScrollPosition = m_ScrollRect.normalizedPosition;
-        }
+        if (!_mouseHover)
+            Autoscroll();
     }
-    void InputScroll()
-    {
-        if (m_Selectables.Count > 0)
-        {
-            if (Input.GetButtonDown("Horizontal") || Input.GetButtonDown("Vertical") || Input.GetButton("Horizontal") || Input.GetButton("Vertical"))
-            {
-                ScrollToSelected(false);
-            }
-        }
-    }
-    void ScrollToSelected(bool quickScroll)
-    {
-        int selectedIndex = -1;
-        Selectable selectedElement = EventSystem.current.currentSelectedGameObject ? EventSystem.current.currentSelectedGameObject.GetComponent<Selectable>() : null;
 
-        if (selectedElement)
+
+    public void Autoscroll()
+    {
+        if (contentPanel == null)
+            contentPanel = GetComponent<ScrollRect>().content;
+
+        GameObject selected = EventSystem.current.currentSelectedGameObject;
+
+        if (selected == null)
         {
-            selectedIndex = m_Selectables.IndexOf(selectedElement);
+            return;
         }
-        if (selectedIndex > -1)
+        if (selected.transform.parent != contentPanel.transform)
         {
-            if (quickScroll)
-            {
-                m_ScrollRect.normalizedPosition = new Vector2(0, 1 - (selectedIndex / ((float)m_Selectables.Count - 1)));
-                m_NextScrollPosition = m_ScrollRect.normalizedPosition;
-            }
-            else
-            {
-                m_NextScrollPosition = new Vector2(0, 1 - (selectedIndex / ((float)m_Selectables.Count - 1)));
-            }
+            return;
         }
+        if (selected == lastSelected)
+        {
+            return;
+        }
+
+        selectedRectTransform = (RectTransform)selected.transform;
+        targetPos.x = contentPanel.anchoredPosition.x;
+        targetPos.y = -(selectedRectTransform.localPosition.y) - (selectedRectTransform.rect.height / 2);
+        targetPos.y = Mathf.Clamp(targetPos.y, 0, contentPanel.sizeDelta.y - scrollRectTransform.sizeDelta.y);
+
+        contentPanel.anchoredPosition = targetPos;
+        lastSelected = selected;
     }
+
+    bool _mouseHover;
     public void OnPointerEnter(PointerEventData eventData)
     {
-        mouseOver = true;
+        _mouseHover = true;
     }
+
     public void OnPointerExit(PointerEventData eventData)
     {
-        mouseOver = false;
-        ScrollToSelected(false);
+        _mouseHover = false;
     }
 }
