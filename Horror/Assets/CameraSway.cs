f8c039ccba87a31a1ba572df9ba3eb0055c9b4cf
Seth Florman <18.seth.florman@ksd.org>
2018-05-23 13:49:00 -0700

    Level Design. SF

---
+++ Horror/Assets/CameraSway.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSway : MonoBehaviour {
+
+    // Use this for initialization
+
+    public float smooth = 3.0f;
+    public float tiltAngle = 5.0f;
+
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float tilt = -Input.GetAxis("Mouse X") * tiltAngle;
+        Quaternion targetRotation = Quaternion.Euler(0f, tilt, tilt);
+        transform.localRotation = Quaternion.Slerp(transform.localRotation, targetRotation, Time.deltaTime * smooth);
+
+    }
+}
